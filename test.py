afc98bd288c6ab2f03c899f7d589859eada16bd6
Simon Bilgeri <Simon.Bilgeri@tum.de>
2014-10-01 17:41:53 +0100

    First commit

---
+++ test.py
@@ -0,0 +1,9 @@
+print('Welome to Simons python program')
+string=input('Please enter a string and see what happens:')
+a=string[::-1].capitalize()
+print(a)
+if a=='Nirolf':
+	print('Hey Florin!')
+else:
+	print('you are not Florin!')
+
