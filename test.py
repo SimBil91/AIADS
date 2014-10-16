71563593bc3808b8752e3477ebefd86fdc63888c
Simon Bilgeri <Simon.Bilgeri@tum.de>
2014-10-16 23:59:30 +0100

    Final changes

--- test.py
+++
@@ -1,9 +0,0 @@
-print('Welome to Simons python program')
-string=input('Please enter a string and see what happens:')
-a=string[::-1].capitalize()
-print(a)
-if a=='Nirolf':
-	print('Hey Florin!')
-else:
-	print('you are not Florin!')
-
